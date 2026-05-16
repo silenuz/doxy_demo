0769955205b3de46ba785b8173ca48c85dbed93e
silenuznowan <horuuendillus@gmail.com>
2026-05-16 15:44:43 -0400

    teest

--- src/traffic_light.h
+++ src/traffic_light.h
@@ -2,15 +2,15 @@
  * This source code was "borrowed" from the Godot documentation.
  * © Copyright 2014-2026, Juan Linietsky, Ariel Manzur and the Godot community (CC-BY 3.0)
  *
-*/
+ */
 
 #ifndef GODOT_CPP_TEMPLATE_TRAFFIC_LIGHT_H
 #define GODOT_CPP_TEMPLATE_TRAFFIC_LIGHT_H
 #pragma once
 
-#include<godot_cpp//classes/control.hpp>
-#include<godot_cpp/classes/texture_rect.hpp>
-#include<godot_cpp/classes/texture2d.hpp>
+#include <godot_cpp //classes/control.hpp>
+#include <godot_cpp/classes/texture2d.hpp>
+#include <godot_cpp/classes/texture_rect.hpp>
 
 using namespace godot;
 
@@ -18,11 +18,10 @@ using namespace godot;
  * TrafficLightType enumerator
  * the traffic light enumerator is used to track the current state of the light (Go,Caution,Stop)
  */
-enum TrafficLightType
-{
-    TRAFFIC_LIGHT_GO, /**< Represents a light indicating Go*/
-    TRAFFIC_LIGHT_CAUTION, /**< Represents a light indicating Caution*/
-    TRAFFIC_LIGHT_STOP /**< Represents a light indicating Stop*/
+enum TrafficLightType {
+	TRAFFIC_LIGHT_GO, /**< Represents a light indicating Go*/
+	TRAFFIC_LIGHT_CAUTION, /**< Represents a light indicating Caution*/
+	TRAFFIC_LIGHT_STOP /**< Represents a light indicating Stop*/
 };
 
 /**
@@ -33,23 +32,23 @@ enum TrafficLightType
  * Godot does not support multiple inheritance for GDExtension classes.
  *
  */
-class TrafficLight : public godot::Control
-{
-    GDCLASS(TrafficLight, Control);
+class TrafficLight : public godot::Control {
+	GDCLASS(TrafficLight, Control);
 
-    TextureRect *texture_rect;
+	TextureRect *texture_rect;
 
-    Ref<Texture2D> go_texture; /**< The Texture2D used for displaying the Go state */
-    Ref<Texture2D> stop_texture;/**< The Texture2D used for displaying the Stop state */
-    Ref<Texture2D> caution_texture;/**< The Texture2D used for displaying the Caution state */
+	Ref<Texture2D> go_texture; /**< The Texture2D used for displaying the Go state */
+	Ref<Texture2D> stop_texture; /**< The Texture2D used for displaying the Stop state */
+	Ref<Texture2D> caution_texture; /**< The Texture2D used for displaying the Caution state */
 
-    TrafficLightType light_type;/**< The current state (Go,Caution,Stop) */
+	TrafficLightType light_type; /**< The current state (Go,Caution,Stop) */
 
 protected:
 	/**
+	 ** nnnnn
 	 * You must declare a protected static void _bind_methods() function in your class header.
 	 */
-    static void _bind_methods();
+	static void _bind_methods();
 	/**
 	 * Primary method used to handle engine-level callbacks, such as an object's life cycle events or node status changes
 	 * @param p_what the notification value from the Godot engine some common ones are READY,PROCESS,ENTER_TREE, and EXIT_TREE
@@ -58,11 +57,9 @@ protected:
 
 public:
 	/**
-	 * @brief constructor
 	 *
-	 *  Create a new instance of the TrafficLight class
 	 */
-    TrafficLight();
+	TrafficLight();
 
 	/**
 	 * Sets the Texture2D to be used when the light is in a Go state
@@ -80,7 +77,7 @@ public:
 	 * Sets the Texture2D to be used when the light is in a Caution state
 	 * @param p_texture the texture to be used
 	 */
-    void set_caution_texture(const Ref<Texture2D> &p_texture);
+	void set_caution_texture(const Ref<Texture2D> &p_texture);
 
 	/**
 	 * Gets the Texture2D that acts as the texture for the Caution state
@@ -92,7 +89,7 @@ public:
 	 * Sets the Texture2D to be used when the light is in a Stop state
 	 * @param p_texture the texture to be used
 	 */
-    void set_stop_texture(const Ref<Texture2D> &p_texture);
+	void set_stop_texture(const Ref<Texture2D> &p_texture);
 
 	/**
 	 * Gets the Texture2D that acts as the texture for the Stop state
@@ -101,9 +98,9 @@ public:
 	Ref<Texture2D> get_stop_texture() const;
 
 	/**
-     *  Sets the current state of the light, go, caution, stop
-     * @param p_light_type the traffic light type enumerator
-     */
+	 *  Sets the current state of the light, go, caution, stop
+	 * @param p_light_type the traffic light type enumerator
+	 */
 	void set_light_type(TrafficLightType p_light_type);
 
 	/**
