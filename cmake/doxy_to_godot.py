52cc3afffea6da7f969594e4687263617198122a
silenuznowan <horuuendillus@gmail.com>
2026-05-22 14:21:37 -0400

    fixed part of output problems

--- cmake/doxy_to_godot.py
+++ cmake/doxy_to_godot.py
@@ -257,7 +257,7 @@ def parse_xml_text(doxygen_node: et.Element) -> str:
                 parts.append(content)
             elif mixed_element_node.tag == "godotonly":
                 if mixed_element_node.get('position') == "close":
-                    parts[-1] = parts[-1] + mixed_element_node.get("content")
+                    parts[-1] = parts[-1] + mixed_element_node.get("content") + mixed_element_node.tail
                 else:
                     parts.append(mixed_element_node.get("content") + mixed_element_node.tail.strip())
 
