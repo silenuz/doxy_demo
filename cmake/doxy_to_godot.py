315a11f180732c12fce891f2e03068328cc57bb3
silenuznowan <horuuendillus@gmail.com>
2026-05-22 19:10:50 -0400

    small fix to tag text parser where the tail was sometime omitted.

--- cmake/doxy_to_godot.py
+++ cmake/doxy_to_godot.py
@@ -285,8 +285,10 @@ def parse_xml_text(doxygen_node: et.Element) -> str:
             else:
                 parts.append(mixed_element_node.get("content") + mixed_element_node.tail.strip())
 
-        if not mixed_element_node.tail is None:
-            if not mixed_element_node.tag == "godotonly" and not mixed_element_node.tail == ' ':
+        if not mixed_element_node.tail is None and not mixed_element_node.tail is None:
+            if mixed_element_node.tag == "godotonly":
+                parts[-1] = parts[-1] + mixed_element_node.tail.strip()
+            else:
                 parts.append(mixed_element_node.tail.strip())
 
     text = " ".join(parts)
